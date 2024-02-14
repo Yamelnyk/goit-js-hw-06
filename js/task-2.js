aed075b8a48616b99b8b8cbfcb5d1a77045229b3
Yamelnyk <yachkana@ukr.net>
2024-02-15 01:16:19 +0200

    all task

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter((item) => item !== itemToRemove);
+
+    // for (let i = 0; i <= this.#items.length; i++) {
+    //   if (itemToRemove === this.#items[i]) {
+    //     const updateItems = this.#items.splice(i, 1);
+    //     // return updateItems;
+    //   }
+    // }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
